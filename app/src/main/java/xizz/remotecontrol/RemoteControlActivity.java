e57264883d681d5363e339dbdac4ced71252154a
Xi Wei <xwei@utexas.edu>
2015-05-27 14:10:48 -0400

    New Project

---
+++ app/src/main/java/xizz/remotecontrol/RemoteControlActivity.java
@@ -0,0 +1,13 @@
+package xizz.remotecontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class RemoteControlActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_remote_control);
+	}
+}
